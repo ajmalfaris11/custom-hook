d304222b95971cfeea5a52c46f8660d45c52cc0d
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-30 18:33:35 +0530

    Custom hook created for counter functionalities are incremnt when click the + button decrement when click - button

--- src/app/Counter.js
+++ src/app/Counter.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function Counter() {
-    const [number, setNumber] = useState(0);
-    const increment = ()=> setNumber(i=>i+1);
-    const decrement = ()=> setNumber(i=>i-1);
+  const [number, increment, decrement] = useCounter();
+
   return (
-    <div className='counter'>
-        <button onClick={decrement}>-</button>
-        <h1>{number}</h1>
-        <button onClick={increment}>+</button>
+    <div className="counter">
+      <button onClick={decrement}>-</button>
+      <h1>{number}</h1>
+      <button onClick={increment}>+</button>
     </div>
-  )
+  );
+}
+
+// Custom hook for count
+
+function useCounter() {
+  const [number, setNumber] = useState(0);
+  const increment = () => setNumber((i) => i + 1);
+  const decrement = () => setNumber((i) => (i > 0 ? i - 1 : 0));
+
+  return [number, increment, decrement];
 }
